93bb14e39b95f818d6f47d8bf9763a75a6c1c4f4
Kundo Perez <cundocu@Yahoo.es>
2018-09-21 18:23:57 -0400

    rererrerreer

---
+++ BuildingSecurity/Controllers/DutyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuildingSecurity.BLL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingSecurity.Controllers
+{
+    public class DutyController : Controller
+    {
+
+        IPeopleRepository repository;
+        public DutyController( IPeopleRepository repo)
+        {
+           this.repository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult SearchResidents () => View(new {peoplelist =   repository.People , buildinglist = new List<string> { "101","102","103"} }  );
+    }
+}
